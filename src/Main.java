f0e9103b172158d1e0afd236b3cd4be759121a97
Artem Vozniak <voznyako@gmail.com>
2024-08-11 19:47:04 +0400

    Fixed payment amount

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        int balancePaymentAmount = 3000; // Сумма пополнения
+        int balancePaymentAmount = 1000; // Сумма пополнения
         int initialBalance = 160; // Баланс клиента до пополнения
         int bonus = 0; //Количество бонусов
         int rubPer1Bonus = 100; // Минимальная сумма пополнения для получения 1 бонусного рубля
-        if (balancePaymentAmount >= 1000) {
+        if (balancePaymentAmount > 1000) {
             bonus = balancePaymentAmount / rubPer1Bonus;
         }
         int totalBalance = initialBalance + balancePaymentAmount + bonus;
