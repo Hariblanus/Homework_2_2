ea423a50dbcb6220d369652fecdcbb40ef718a86
Artem Vozniak <voznyako@gmail.com>
2024-08-11 16:39:37 +0400

    Fixed Sytnax

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,10 @@ public class Main {
         int initialBalance = 160; // Баланс клиента до пополнения
         int bonus = 0; //Количество бонусов
         int rubPer1Bonus = 100; // Минимальная сумма пополнения для получения 1 бонусного рубля
-
-        {
-            if (balancePaymentAmount >= 1000)
-                bonus = balancePaymentAmount / rubPer1Bonus;
-        }
+        if (balancePaymentAmount >= 1000)
+            bonus = balancePaymentAmount / rubPer1Bonus;
         int totalBalance = initialBalance + balancePaymentAmount + bonus;
-        {
-            System.out.println("Ваш баланс: " + totalBalance + " рублей.");
-            System.out.println("Начислено " + bonus + " бонусных рублей.");
-        }
+        System.out.println("Ваш баланс: " + totalBalance + " рублей.");
+        System.out.println("Начислено " + bonus + " бонусных рублей.");
     }
 }
