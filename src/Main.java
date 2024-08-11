2ad6388e8c9ee83be05139eb9dd6550c8434f8e3
Artem Vozniak <voznyako@gmail.com>
2024-08-11 15:53:54 +0400

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int balancePaymentAmount = 3000; // Сумма пополнения
+        int initialBalance = 160; // Баланс клиента до пополнения
+        int bonus = 0; //Количество бонусов
+        int rubPer1Bonus = 100; // Минимальная сумма пополнения для получения 1 бонусного рубля
+
+        {
+            if (balancePaymentAmount >= 1000)
+                bonus = balancePaymentAmount / rubPer1Bonus;
+        }
+        int totalBalance = initialBalance + balancePaymentAmount + bonus;
+        {
+            System.out.println("Ваш баланс: " + totalBalance + " рублей.");
+            System.out.println("Начислено " + bonus + " бонусных рублей.");
+        }
     }
 }
